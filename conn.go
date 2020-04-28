7f08e728ac22703ed137a421aaa3161f266765d1
Robin Broda <robin@broda.me>
2020-04-28 21:43:13 +0200

    Add io.ByteScanner to interface

--- conn.go
+++ conn.go
@@ -14,6 +14,7 @@ import (
 
 type Conn interface {
 	net.Conn
+	io.ByteScanner
 }
 
 type conn struct {
--- dial.go
+++ dial.go
@@ -5,10 +5,10 @@ import (
 )
 
 func Dial(network, address string) (c Conn, err error) {
-	c, err = net.Dial(network, address)
+	oC, err := net.Dial(network, address)
 	if err != nil {
 		return nil, err
 	}
-	c, err = Wrap(c)
+	c, err = Wrap(oC)
 	return
 }
